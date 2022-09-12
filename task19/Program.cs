f6f1b02789e9dc2230c73943f09f949119b2087f
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-12 23:10:37 +0300

    Домашнее задание выполнено

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Write("Введите целое число:   ");
+
+string number = Console.ReadLine()!;
+
+int length = number.Length;
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsPalindrome()) Console.WriteLine($"Введное число - {number} - является палиндромом");
+else Console.WriteLine($"Число {number} не является палиндромом");
+
